64d1ae4c2bbfacbcdcd57d3855eed3403e28fe2e
Vera <63463936+lveral@users.noreply.github.com>
2024-08-06 21:58:14 +0500

    add vendors + some code in main.go

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT  is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
