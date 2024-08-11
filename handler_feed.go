d21f518144365832d3458ce95ee02db7a3ac6a8c
Vera <63463936+lveral@users.noreply.github.com>
2024-08-11 14:43:44 +0500

    add files

--- handler_feed.go
+++ handler_feed.go
@@ -4,15 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
-	"github.com/lveral/rssagg/internal/auth"
 	"github.com/lveral/rssagg/internal/database"
 	"net/http"
 	"time"
 )
 
-func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
-		Name string `json:"name""`
+		Name string `json:"name"`
+		URL  string `json:"url"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -22,31 +22,28 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
 	})
 	if err != nil {
-		responseWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		responseWithError(w, 400, fmt.Sprintf("Couldn't create feed: %v", err))
 		return
 	}
 
-	responseWithJSON(w, 201, databaseUserToUser(user))
+	responseWithJSON(w, 201, databaseFeedToFeed(feed))
 }
 
-func (apiCfg *apiConfig) handlerGetFeed(w http.ResponseWriter, r *http.Request) {
-	apiKey, err := auth.GetAPIKey(r.Header)
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
 	if err != nil {
-		responseWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		responseWithError(w, 400, fmt.Sprintf("Couldn't get feeds: %v", err))
 		return
 	}
 
-	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
-	if err != nil {
-		responseWithError(w, 403, fmt.Sprintf("Couldn't get user: %v", err))
-		return
-	}
-	responseWithJSON(w, 200, databaseUserToUser(user))
+	responseWithJSON(w, 201, databaseFeedsToFeeds(feeds))
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,4 +1,7 @@
 -- name: CreateFeed :one
-INSERt INTO feeds (id, created_at, updated_at, name, url, user_id)
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: GetFeeds :many
+SELECT * FROM feeds;
