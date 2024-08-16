87c2c842e7940818618cdd57148db055253c1f57
Vera <63463936+lveral@users.noreply.github.com>
2024-08-17 01:25:17 +0500

    add queries to database

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -5,3 +5,11 @@ RETURNING *;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+-- name: GetNextFeedsToFetch :many
+select * from feeds order by coalesce(last_fetched_at, '1900-01-01');
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET updated_at = CURRENT_TIMESTAMP, last_fetched_at = CURRENT_TIMESTAMP
+WHERE id = $1;
